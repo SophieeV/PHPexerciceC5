3664c7f93ea784d25281affce72629410134b43f
SophieeV <sophievesin@wanadoo.fr>
2021-06-14 11:58:52 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exerciceC5</title>
+</head>
+<body>
+
+<?php 
+function stringNumber(){
+    $string = 'je suis une chaîne de caractères ';
+    $number = rand(1, 20);
+echo $string .$number;
+
+}
+
+stringNumber();
+
+
+?>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
